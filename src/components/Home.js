62e18163302ecab2dc2bbd776be38c4ba3419cf6
Andrea Elvegård <andrea.elvegaard@yahoo.no>
2022-09-07 08:14:57 +0200

    Added most important things to home page

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import profileImage from '../assets/profilepicture.png';
+
+function Home() {
+    return (
+      <section className="home" id="home">
+        <div style={{backgroundImage: `url(${profileImage})`}} className="home_picture"></div>
+        <div className="home_text">
+            <h1 className="home_title">FULLSTACK DEVELOPER</h1>
+            <p className="gray home_location">Located in Oslo📍</p>
+        </div>
+        {/* <div className="home_carousel">
+            <p>Andrea Elvegård</p>
+            <p>Andrea Elvegård</p>
+        </div> */}
+      </section>
+    );
+  }
+  
+  export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav className="nav nav_open">
+        <a className="nav_link" href="#home">Home</a>
+        <a className="nav_link" href="#">About</a>
+        <a className="nav_link" href="#">Projects</a>
+        <a className="nav_link" href="#">Contact</a>
+        <a className="nav_link" href="#">CV</a>
+    </nav>
+  );
+}
+
+export default Navbar;
