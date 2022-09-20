e7131e36df66c71c7dbec6a4a64f9e9754db4bd2
Andrea Elvegård <andrea.elvegaard@yahoo.no>
2022-09-20 10:28:50 +0200

    fetching repos from github

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,18 +1,19 @@
-function Project() {
+function Project({ title, description, updatedAt, githubUrl, liveUrl, index }) {
+    if(liveUrl !== null && liveUrl !== "") console.log(liveUrl);
     return (
         <div className="work_project-container project-left">
             <div className="project_title-container">
-                <p className="project_num">01</p>
-                <h3 className="project_title gray">Portfolio Website</h3>
+                <p className="project_num">{index}</p>
+                <h3 className="project_title gray">{title}</h3>
             </div>
             <p className="project_text gray">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu ultricies leo. Proin laoreet lectus ut ligula condimentum, a bibendum magna interdum.
+                {description || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu ultricies leo. Proin laoreet lectus ut ligula condimentum, a bibendum magna interdum.'}
             </p>
             <p className="project_date gray">
-                10. Aug 2022
+                {updatedAt}
             </p>
-            <button className="project_btn">LIVE</button>
-            <button className="project_btn">GITHUB</button>
+            {(liveUrl !== null && liveUrl !== "") && <button className="project_btn" onClick={() => window.open(liveUrl, '_blank')}>LIVE</button>}
+            <button className="project_btn" onClick={() => window.open(githubUrl, '_blank')}>GITHUB</button>
         </div>
     )
 }
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,53 @@
 import Project from "./Project";
+import { Octokit } from "octokit";
+import { useState, useEffect } from "react";
 
 function Work() {
+    const [loading, setLoading] = useState(true);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        const octokit = new Octokit({
+            auth: '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'
+        });
+        
+        octokit.request('GET /users/{username}/repos', {
+            username: 'andreaelve'
+        })
+            .then(res => res.data)
+            .then(data => {
+                console.log(data[0])
+                const usefulProjectData = data.map((el, i) => {
+                    return { 
+                        github: el.html_url, 
+                        title: el.name,
+                        liveLink: el.homepage,
+                        description: el.description, 
+                        lastUpdated: el.updated_at,
+                        index: `0${i+1}`
+                    }
+                })
+                setProjects(usefulProjectData);
+            })
+            .then(() => setLoading(false))
+            .catch(err => console.log(err));
+    }, []);
+
+    console.log(projects)
+
+    if(loading){
+        return (
+            <section className="work" id="work">
+                 <h2 className="work_title">RECENT PROJECTS</h2>
+                 <p>Loading...</p>
+            </section>
+        )
+    }
+
     return (
         <section className="work" id="work">
             <h2 className="work_title">RECENT PROJECTS</h2>
-            <Project />
+            <Project title={projects[0].title} description={projects[0].description} updatedAt={projects[0].updatedAt} githubUrl={projects[0].github} liveUrl={projects[0].liveLink} index={projects[0].index} />
             <div className="work_project-container project-right">
             <div className="project_title-container">
                 <p className="project_num">02</p>
