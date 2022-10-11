03b4dede07c28f20934798d62fad68dada62187b
Andrea Elvegård <andrea.elvegaard@yahoo.no>
2022-10-11 17:49:34 +0200

    Rydder litt opp i kode og animasjon til sirkler

--- src/components/About.js
+++ src/components/About.js
@@ -14,9 +14,8 @@ const About = () => {
           <h2 className='about_title'>HELLO, I'M ANDREA</h2>
           <p className='about_hand-icon'>👋🏼</p>
         </div>
-       <p className='about_text'>
-       I'm a <span className="red">happy</span> and friendly 'people' person who <span className="red">thrives in creative and social environments</span>. What drove me to pursue a career as a developer is <span className="red">my passion</span> for problem solving and <span className="red">experimenting</span> with new ideas.
-        {/* Lorem ipsum dolor sit amet, <span className="red">consectetur</span> adipiscing elit. Aliquam ac venenatis elit. <span className="red">Quisque mi lectus</span>, blandit nec diam a, auctor facilisis est. Maecenas in tellus at  */}
+        <p className='about_text'>
+          I'm a <span className="red">happy</span> and friendly 'people' person who <span className="red">thrives in creative and social environments</span>. What drove me to pursue a career as a developer is <span className="red">my passion</span> for problem solving and <span className="red">experimenting</span> with new ideas.
         </p>
         <p className='about_text'>
           I'm currently employed as a consultant at Salt, and I'm continuously working towards widening my knowledge and <span className="red">improving</span> my skillset. 
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,15 @@ import profileImage from '../assets/profilepicture.png';
 function Home() {
     return (
       <section className="home" id="home">
-        <div style={{backgroundImage: `url(${profileImage})`}} className="home_picture"></div>
-        <div className="home_text">
-            <h1 className="home_title">FULLSTACK DEVELOPER</h1>
-            <p className="gray home_location">Located in Oslo📍</p>
-        </div>
-        {/* <div className="home_carousel">
-            <p>Andrea Elvegård</p>
-            <p>Andrea Elvegård</p>
-        </div> */}
+          <div className='home_circle-container'>
+            <div className="home_circle-gray"></div>
+            <div className="home_circle-red"></div>
+            <div style={{backgroundImage: `url(${profileImage})`}} className="home_picture"></div>
+          </div>
+          <div className="home_text">
+              <h1 className="home_title">FULLSTACK DEVELOPER</h1>
+              <p className="gray home_location">Located in Oslo📍</p>
+          </div>
       </section>
     );
   }
